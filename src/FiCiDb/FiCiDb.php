1ff99750692d091a8822c01c333e7f39ebbe94c9
TuncayHp2 <tunc270@gmail.com>
2025-07-28 23:12:36 +0300

    up

--- src/FiCiDb/FiCiDb.php
+++ src/FiCiDb/FiCiDb.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace Engtuncay\PhpCiUtils\FiCiDb;
 
 use CodeIgniter\Database\BaseConnection;
+use Engtuncay\Phputils8\FiDto\Fdr;
+use Engtuncay\Phputils8\Pdo\FiQuery;
 
-class FiCiDb {
+class FiCiDb
+{
 
   /**
    * @var BaseConnection|null
@@ -37,8 +41,51 @@ class FiCiDb {
     return $this->db;
   }
 
+  public function sqlExecute(FiQuery $fiQuery): Fdr  //string $sql, ?array $fkbParams = null
+  {
+    $fdr = new Fdr();
+    $fkbParams = $fiQuery->getFkbParams();
 
+    //$stmt = $this->prepare($fiQuery->getSql());;
+    //$boExec = null;
+
+    $sql = $fiQuery->getSql();
+
+    $query = $this->getDb()->query($sql, $fkbParams->getArr());
+
+    // Query çalıştırma
+    //$query = $db->query("SELECT * FROM users WHERE id = ?", [$userId]);
+
+    // Hata kontrolü
+    if ($query === false) {
+      // Hata var
+      $error = $this->getDb()->error();
+      log_message('error', 'Database error: ' . $error['message']);
+      $fdr->setBoExec(false);
+      //$fdr->setBoResult(false);
+      return $fdr;
+    }
+
+    // Sonuçları alma
+    // $results = $query->getResult();        // Object array
+    $results = $query->getResultArray();   // Associative array
+    // $row = $query->getRow();              // Tek satır object
+    // $rowArray = $query->getRowArray(); 
+
+    //
+
+    // if ($fkbParams != null) {
+    //   $boExec = $stmt->execute($fkbParams->getParams());
+    // } else {
+    //   $boExec = $stmt->execute();
+    // }
+
+    $fdr->setBoExec(true);
+    $fdr->setBoResult(true);
+    // $fkbResult = FiKeybean::bui($stmt->fetch(PDO::FETCH_ASSOC));
+    $fdr->setRefValue($results);
+    // $fdr->setRefValue($fkbResult);
+
+    return $fdr;
+  }
 }
-
-
-?>
